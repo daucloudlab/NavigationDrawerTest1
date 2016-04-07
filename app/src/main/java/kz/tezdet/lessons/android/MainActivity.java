adebcb3646f711825d009043ba6a4ac11e33e1ee
daulet <daucloudlab@gmail.com>
2016-04-07 11:47:12 +0600

    version 02

--- app/src/main/java/kz/tezdet/lessons/android/MainActivity.java
+++ app/src/main/java/kz/tezdet/lessons/android/MainActivity.java
@@ -2,30 +2,71 @@ package kz.tezdet.lessons.android;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout ;
+    Toolbar toolbar ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        initNavigationDrawer();
+
+    }
+
+    public void initNavigationDrawer(){
+        NavigationView navigationView = (NavigationView)findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public boolean onNavigationItemSelected(MenuItem item) {
+                int id = item.getItemId() ;
+                switch (id){
+                    case R.id.home:
+                        Toast.makeText(getApplicationContext(), "HOME", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.settings:
+                        Toast.makeText(getApplicationContext(), "SETTINGS", Toast.LENGTH_SHORT).show();
+                        break ;
+                }
+                return true;
             }
         });
+        View header = navigationView.getHeaderView(0) ;
+        TextView emailTextView = (TextView)header.findViewById(R.id.tv_email) ;
+        emailTextView.setText("dauletbayymbetov@gmail.com");
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer) ;
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,
+                drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer){
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+//        drawerLayout.addDrawerListener() ;
+        actionBarDrawerToggle.syncState();
     }
 
     @Override
@@ -38,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
+        // automatically handle clicks on the home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
